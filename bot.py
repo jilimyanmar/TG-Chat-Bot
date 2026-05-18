1a4d39e086dbbba248c7518d0d93dd462116f105
jilimyanmar <jilimyanmar@gmail.com>
2026-05-18 23:58:50 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-import os
 
-TOKEN = os.getenv("8963669087:AAEBAXUFOOVdGWBdGhkyby6e5VADABYvMwM")
+TOKEN = "8963669087:AAEBAXUFOOVdGWBdGhkyby6e5VADABYvMwM"
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text.lower()
+    text = update.message.text.lower().strip()
 
     if "hi" in text:
         await update.message.reply_text("မင်္ဂလာပါ 👋")
@@ -14,7 +13,6 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Price - 5000 MMK")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
 app.run_polling()
